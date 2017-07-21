79689fc37377c1ccda5b9244e4d8c98af183e0be
grhong <georgehong1997@gmail.com>
2017-07-21 16:38:22 -0700

    Changed Hero Jump Mechanic
    hero will move back to the edge and then jump. The hero will not bounce off the block it hits. There is now a high score. Still need to fix the block placement maybe.

--- StairTris/StairTris/GameScene.swift
+++ StairTris/StairTris/GameScene.swift
@@ -52,20 +52,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var hero: SKSpriteNode!
     var gameOver: SKSpriteNode!
     var restartButton: MSButtonNode!
-    var clearScreen: SKSpriteNode!
+    var mainMenuButton: MSButtonNode!
+    var dieNowButton: MSButtonNode!
     var scrollLayer: SKNode!
     var offset: CGFloat = 0
-    var canShake = true
-    var jumpPower: CGFloat = 8
+    var canShake = false
+    var jumpPower: CGFloat = 10
     var jumping = false
     var lastGround: SKSpriteNode!
+    var highScoreLabel: SKLabelNode!
+    var sidePower: CGFloat = 3
+    var dead = false
+    var highScore: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: "highScore")
+        }
+        set(high) {
+            UserDefaults.standard.set(high, forKey: "highScore")
+        }
+    }
     
     override func didMove(to view: SKView) {
         gridNode = childNode(withName: "//gridNode") as! Grid
         scoreLabel = childNode(withName: "scoreLabel") as! SKLabelNode
         timerLabel = childNode(withName: "timerLabel") as! SKLabelNode
         rotateArea = childNode(withName: "rotateArea") as! SKSpriteNode
-        hero = childNode(withName: "hero") as! SKSpriteNode
+        hero = childNode(withName: "//hero") as! SKSpriteNode
         gameOver = childNode(withName: "gameOver") as! SKSpriteNode
         gameOver.isHidden = true
         restartButton = childNode(withName: "//restartButton") as! MSButtonNode
@@ -74,21 +86,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             scene?.scaleMode = .aspectFit
             view.presentScene(scene)
         }
-        clearScreen = childNode(withName: "clearScreen") as! SKSpriteNode
-        clearScreen.isHidden = true
+        mainMenuButton = childNode(withName: "//mainMenuButton") as! MSButtonNode
+        mainMenuButton.selectedHandler = {
+            let scene = MainMenu(fileNamed: "MainMenu")
+            scene?.scaleMode = .aspectFit
+            view.presentScene(scene)
+        }
+        dieNowButton = childNode(withName: "//dieNowButton") as! MSButtonNode
+        dieNowButton.selectedHandler = {
+            self.gameOver.isHidden = false
+            self.isPaused = true
+            self.dead = true
+        }
         pieceArray = childNode(withName: "arrayNode") as! ArrayNode
         scrollLayer = childNode(withName: "//scrollLayer")!
+        highScoreLabel = childNode(withName: "//highScoreLabel") as! SKLabelNode
         
         /*
-        let jump = SKAction.run() {
-            let hero = self.hero
-            hero?.physicsBody?.applyImpulse(CGVector(dx: 2, dy: self.jumpPower))
-        }
-        let moveBack = SKAction(named: "movePlayerBack")!
-        let takeOff = SKAction.sequence([moveBack, jump])
-        takeOff.speed = CGFloat(1.0/(timeLimit-1))
-        hero.run(takeOff)
-        */
+         let jump = SKAction.run() {
+         let hero = self.hero
+         hero?.physicsBody?.applyImpulse(CGVector(dx: 2, dy: self.jumpPower))
+         }
+         let moveBack = SKAction(named: "movePlayerBack")!
+         let takeOff = SKAction.sequence([moveBack, jump])
+         takeOff.speed = CGFloat(1.0/(timeLimit-1))
+         hero.run(takeOff)
+         */
         
         pieceArray.setUpArray()
         
@@ -111,12 +134,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             if contactB.categoryBitMask == 4 || contactA.categoryBitMask == 4 {
                 gameOver.isHidden = false
                 self.isPaused = true
-                if currentState == .normal {
-                    clearScreen.isHidden = false
-                }
+                dead = true
             }
             else if contactB.categoryBitMask == 2 || contactA.categoryBitMask == 2 {
-                //jumping = false
+                if contactA.categoryBitMask == 1 {
+                    contactA.velocity = CGVector(dx: 0, dy: 0)
+                    
+                }
+                else {
+                    contactB.velocity = CGVector(dx: 0, dy: 0)
+                }
+                jumpPower = 8
+                sidePower = 3
             }
         }
     }
@@ -126,7 +155,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let location = touch.location(in: self)
         let nodeAtPoint = atPoint(location)
         if nodeAtPoint.name == "gameOver" { return }
-        if !touching {
+        if !touching && !dead {
             pieceArray.array[0].removeFromParent()
             piece = pieceArray.array[0]
             piece.xScale = 1
@@ -148,7 +177,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if piece != nil && secondFinger != true {
+        if piece != nil && secondFinger != true && !dead {
             let touch = touches.first!
             let location = touch.location(in: self)
             piece.position = location
@@ -170,6 +199,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 piece.removeFromParent()
                 piece = nil
                 touching = false
+                let sound = SKAction.playSoundFileNamed("NFF-menu-03-a", waitForCompletion: false)
+                self.run(sound)
             }
         }
     }
@@ -182,14 +213,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func resetTimer() {
         /*
-        let jump = SKAction.run() {
-            let hero = self.hero
-            hero?.physicsBody?.applyImpulse(CGVector(dx: 2, dy: self.jumpPower))
-        }
-        let moveBack = SKAction(named: "movePlayerBack")!
-        moveBack.speed = CGFloat(1.0/(timeLimit-1))
-        let takeOff = SKAction.sequence([moveBack, jump])
-        hero.run(takeOff)*/
+         let jump = SKAction.run() {
+         let hero = self.hero
+         hero?.physicsBody?.applyImpulse(CGVector(dx: 2, dy: self.jumpPower))
+         }
+         let moveBack = SKAction(named: "movePlayerBack")!
+         moveBack.speed = CGFloat(1.0/(timeLimit-1))
+         let takeOff = SKAction.sequence([moveBack, jump])
+         hero.run(takeOff)*/
+        hero?.physicsBody?.applyImpulse(CGVector(dx: sidePower, dy: self.jumpPower))
+        jumping = false
         scrollTimer = timeLimit
         offset = 0
     }
@@ -197,36 +230,41 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func shake() {
         if canShake {
             print("I am SHOOK")
-            jumping = false
-            jumpPower += 4
+            jumpPower = 12
+            sidePower = 2
         }
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        let stairs = nodes(at: CGPoint(x: hero.position.x + 15, y: hero.position.y))
-        for node in stairs {
-            if node.physicsBody?.categoryBitMask == 2 && !jumping {
-                hero.physicsBody?.applyImpulse(CGVector(dx: 0.25, dy: self.jumpPower))
-                if jumpPower != 8 {
-                    jumpPower = 8
-                }
-                jumping = true
-            }
-        }
-        let ground = nodes(at: CGPoint(x: hero.position.x, y: hero.position.y - 20))
+    override func update(_ currentTime: TimeInterval) {/*
+         let stairs = nodes(at: CGPoint(x: hero.position.x + 15, y: hero.position.y))
+         for node in stairs {
+         if node.physicsBody?.categoryBitMask == 2 && !jumping {
+         hero.physicsBody?.applyImpulse(CGVector(dx: 0.25, dy: self.jumpPower))
+         jumping = true
+         }
+         }
+        let ground = nodes(at: CGPoint(x: hero.position.x, y: hero.position.y - 15))
         for node in ground {
             if node.physicsBody?.categoryBitMask == 2 {
                 if node != lastGround {
-                    lastGround = node as! SKSpriteNode
-                    jumping = false
+                    if hero.physicsBody?.velocity == CGVector(dx: 0, dy: 0) {
+                        print("\(lastGround)")
+                        lastGround = node as! SKSpriteNode
+                        jumping = false
+                        //next 2 lines are for the move back then leap method
+                        let moveBack = SKAction(named: "movePlayerBack")!
+                        hero.run(moveBack)
+                    }
                 }
             }
-        }
-        /*
-        if hero.physicsBody?.velocity == CGVector(dx:0 , dy: 0) {
-            jumping = false
         }*/
+        
         // Called before each frame is rendered
+        if scrollTimer < 4 && !jumping {
+            jumping = true
+            let moveBack = SKAction(named: "movePlayerBack")!
+            hero.run(moveBack)
+        }
         scrollTheLayer()
         scrollTimer -= fixedDelta
         timerLabel.text = String(Int(scrollTimer))
@@ -240,5 +278,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             resetTimer()
             gridNode.scrollCells()
         }
+        if score > highScore {
+            highScore = score
+        }
+        highScoreLabel.text = String(highScore)
     }
 }
